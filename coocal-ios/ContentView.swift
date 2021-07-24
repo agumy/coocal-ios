6b3fff90be74541be3a0287ad2d34e7f1701a673
agumy <nov13sh@gmail.com>
2021-07-24 22:30:29 +0900

    Initial Commit

---
+++ coocal-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coocal-ios
+//
+//  Created by agumy on 2021/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coocal-ios/coocal_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  coocal_iosApp.swift
+//  coocal-ios
+//
+//  Created by agumy on 2021/07/24.
+//
+
+import SwiftUI
+
+@main
+struct coocal_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
