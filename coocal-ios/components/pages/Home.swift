6e807a2eed174cd5ebfc5791e66e1e51f5def694
agumy <nov13sh@gmail.com>
2021-07-24 22:53:19 +0900

    Add page

---
+++ coocal-ios/components/pages/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  coocal-ios
+//
+//  Created by agumy on 2021/07/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
