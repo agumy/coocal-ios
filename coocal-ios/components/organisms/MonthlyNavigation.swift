8dcbd1602d6cda5cecd84b7ea6bb33aedbd6de41
agumy <nov13sh@gmail.com>
2021-08-12 20:51:06 +0900

    add binding state

--- coocal-ios/components/organisms/MonthlyNavigation.swift
+++ coocal-ios/components/organisms/MonthlyNavigation.swift
@@ -8,22 +8,26 @@
 import SwiftUI
 
 struct MonthlyNavigation: View {
+    @Binding var targetDate: Date
+    
     var body: some View {
         VStack {
             HStack {
-                Text("←")
-                Text("2021/07")
-                Text("→")
+                Button(action: {}, label: {Text("←")})
+                Text(DateFormatter.month.string(from: targetDate))
+                Button(action: {}, label: {Text("→")})
                 Spacer()
                 Text("Today")
             }
+            .foregroundColor(.black)
         }
         .padding()
     }
 }
 
 struct MonthlyNavigation_Previews: PreviewProvider {
+    @State static var targetDate = Date()
     static var previews: some View {
-        MonthlyNavigation()
+        MonthlyNavigation(targetDate: $targetDate)
     }
 }
